36679a5ce7825abf60377b3791e5d38fc138b625
Rodrigo Corsi <rodrigocorsi@gmail.com>
2017-09-17 08:12:35 -0300

    NewInit and remove HasError

--- validate.go
+++ validate.go
@@ -1,5 +1,7 @@
 package vld
 
+import "fmt"
+
 type Validate struct {
 	errs      Errors
 	fieldName string
@@ -10,22 +12,27 @@ func New() *Validate {
 	return &Validate{}
 }
 
+func NewInit(fieldName string) *Validate {
+	return &Validate{
+		fieldName: fieldName,
+	}
+}
+
 func (v *Validate) Init(fieldName string) *Validate {
 	v.fieldName = fieldName
 	v.message = ""
 	return v
 }
 
-func (v *Validate) HasError() bool {
-	return v.errs != nil
-}
-
 func (v *Validate) Err() error {
-	return v.errs
+	if v.errs != nil {
+		return v.errs
+	}
+	return nil
 }
 
-func (v *Validate) AddErrMsg(message string) {
-	v.message += message
+func (v *Validate) AddErrMsg(message string, a ...interface{}) {
+	v.message += fmt.Sprintf(message, a...)
 }
 
 func (v *Validate) Ok() bool {
