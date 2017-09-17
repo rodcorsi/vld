dcf7a16bc04813d0c24161b80fd487281d567536
Rodrigo Corsi <rodrigocorsi@gmail.com>
2017-09-17 08:11:34 -0300

    fix error message

--- error.go
+++ error.go
@@ -18,15 +18,17 @@ func NewErrorTag(fieldName, message string) *ErrorTag {
 }
 
 func (e *ErrorTag) Error() string {
-	return fmt.Sprintf("%v: %v\n", e.fieldName, e.message)
+	return fmt.Sprintf("validation for '%v' failed%v", e.fieldName, e.message)
 }
 
 type Errors []*ErrorTag
 
 func (e Errors) Error() string {
 	var buffer bytes.Buffer
-	for _, et := range e {
-		buffer.WriteString(et.Error())
+	buffer.WriteString(e[0].Error())
+	for i := 1; i < len(e); i++ {
+		buffer.WriteString("\n")
+		buffer.WriteString(e[i].Error())
 	}
 	return buffer.String()
 }
